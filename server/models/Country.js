9aceca7ae1e6f7e74396a6a874ed68d181e93ff3
Ziv Kaplan <zivkaplan@gmail.com>
2021-07-31 23:50:51 +0300

    seed database

---
+++ server/models/Country.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const countrySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    capital: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Country', countrySchema);
---
+++ server/seeds/index.js
@@ -0,0 +1,51 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const mongoose = require('mongoose');
+const Country = require('../models/Country');
+
+const countriesJson = require('../seeds/countries.json');
+const countries = countriesJson.countries;
+
+mongoose.connect(
+    process.env.DB_URL || 'mongodb://localhost:27017/react-db-REST',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console.error, 'conection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const seedDb = async () => {
+    for (let country of countries) {
+        country = new Country({
+            name: country.countryName,
+            capital: country.capital,
+        });
+        await country.save();
+    }
+};
+
+const dropReviewsDb = async () => {
+    mongoose.connection.dropCollection('countries', function (err, result) {
+        if (result) {
+            console.log('Collection dropped');
+        } else {
+            console.log('Collection Not Found');
+        }
+    });
+};
+
+dropReviewsDb()
+    .then(() => seedDb())
+    .then(() => {
+        mongoose.connection.close();
+        console.log('Database closed');
+    });
