bdf721ea28f187d7d3c697835471d5ae328b6577
Ziv Kaplan <zivkaplan@gmail.com>
2021-08-03 15:46:54 +0300

    delete path.join in app.use

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const mongooseConfig = (function () {
 // routes
 app.use('/api/countries', defaultRouter);
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve('client', 'build', 'index.html'));
